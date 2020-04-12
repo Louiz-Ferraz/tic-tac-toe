231751b0f3ffad436a10260b4081c65b3a7bfa75
Louiz-Ferraz <luizfelipeferraz1@gmail.com>
2020-04-12 13:57:31 -0300

    Imagens ok
    Todas as 3 imagens desenhadas corretamente

--- jogo.js
+++ jogo.js
@@ -1,11 +1,13 @@
 console.log('Tic-tac-toe --- Louiz-Ferraz');
 
 const board = new Image();
-board.src = './images/board2.png';
+board.src = './images/board.png';
+
+const pecas = new Image();
+pecas.src = './images/pecas2.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
-
 
 /* contexto.drawImage(  //Exemplo de como funciona drawImage
                         (https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage)
@@ -16,6 +18,9 @@ const contexto = canvas.getContext('2d');
     dWidth, dHeight     //Tamanho da imagem dentro do canvas
 ); */
 
+//
+//Imagens
+//
 
 //Board
 const telaBoard = {
@@ -38,46 +43,101 @@ const telaBoard = {
 
 //Peça 1 - X
 const peca1 = {
-    sX: 57,
-    sY: 20,
-    w: 285,
-    h: 285,
-    x: canvas.width/2 - 285/2,
-    y: canvas.height/2 - 100,
+    sX: 15,
+    sY: 249,
+    w: 73,
+    h: 73,
+    x: 33,
+    y: 297,
     desenha() {
         contexto.drawImage(
-            board,
-            telaBoard.sX, telaBoard.sY,
-            telaBoard.w, telaBoard.h,
-            telaBoard.x, telaBoard.y,
-            telaBoard.w, telaBoard.h,
+            pecas,
+            peca1.sX, peca1.sY,
+            peca1.w, peca1.h,
+            peca1.x, peca1.y,
+            peca1.w, peca1.h,
         );
     }
 }
 
 //Peça 2 - O
 const peca2 = {
-    sX: 57,
-    sY: 20,
-    w: 285,
-    h: 285,
-    x: canvas.width/2 - 285/2,
-    y: canvas.height/2 - 100,
+    sX: 15,
+    sY: 15,
+    w: 73,
+    h: 73,
+    x: 33,
+    y: 205,
     desenha() {
         contexto.drawImage(
-            board,
-            telaBoard.sX, telaBoard.sY,
-            telaBoard.w, telaBoard.h,
-            telaBoard.x, telaBoard.y,
-            telaBoard.w, telaBoard.h,
+            pecas,
+            peca2.sX, peca2.sY,
+            peca2.w, peca2.h,
+            peca2.x, peca2.y,
+            peca2.w, peca2.h,
         );
     }
+}
+
+//
+//Posições
+//
+const posicao1 = {
+    x: 33,
+    y: 114,
+}
+
+const posicao2 = {
+    x: 124,
+    y: 114,
+}
+
+const posicao3 = {
+    x: 216,
+    y: 114,
+}
+
+const posicao4 = {
+    x: 33,
+    y: 205,
+}
+
+const posicao5 = {
+    x: 124,
+    y: 205,
+}
+
+const posicao6 = {
+    x: 216,
+    y: 205,
+}
+
+const posicao7 = {
+    x: 33,
+    y: 297,
+}
+
+const posicao8 = {
+    x: 124,
+    y: 297,
+}
+
+const posicao9 = {
+    x: 216,
+    y: 297,
+}
+
+function click() {
+
 }
 
 function loop() {
 
     telaBoard.desenha();
+    peca1.desenha();
+    peca2.desenha();
     requestAnimationFrame(loop); //Função que ajuda a desenhar os quadros de forma inteligente
+    
 }
 
 loop();
